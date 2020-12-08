a05f6707786099a35e1ebef04471e56b6dbc455b
LoganRan625 <75424234+LoganRan625@users.noreply.github.com>
2020-12-08 01:12:18 -0800

    Create turtle_squares.py

---
+++ turtle_squares.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor("black")
+
+for i in range(5):
+    for colours in ["red", "magenta", "blue", "cyan", "green", "yellow", "white"]:
+        turtle.color(colours)
+        turtle.pensize(3)
+        turtle.left(12)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(20)
+        turtle.left(30)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
